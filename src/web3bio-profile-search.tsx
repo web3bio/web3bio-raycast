7d7735edaca7b6cf031dd6c34c82630b260c7bcf
Yan <picturepan2@hotmail.com>
2023-12-07 23:32:28 +0800

    Update

--- src/web3bio-profile-search.tsx
+++ src/web3bio-profile-search.tsx
@@ -1,10 +1,12 @@
 import { Action, ActionPanel, Icon, Image, List, useNavigation } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 import { useEffect, useState } from "react";
-import { handleSearchPlatform } from "./utils/base";
 import { Profile } from "./utils/types";
+import { handleSearchPlatform, SocialPlatformMapping } from "./utils/utils";
+import { PlatformType } from "./utils/platform";
 
 const API_END_POINT = "https://api.web3.bio";
+
 export default function Command() {
   const [searchTerm, setSearchTerm] = useState("");
   const [url, setUrl] = useState(API_END_POINT);
@@ -34,8 +36,8 @@ export default function Command() {
   });
   const emptyText = (() => {
     if (!searchTerm) return "🔍 Search Ethereum (ENS), Lens, Farcaster, UD...";
-    if (searchTerm.length > 0 && !platform) return "❌ Invalid Identity, Please try different identity";
-    if (searchTerm.length > 0 && platform && !profiles?.length) return "👽 No Results, Please try different identity";
+    if (searchTerm.length > 0 && !platform) return "❌ Invalid Identity. Please try different identity.";
+    if (searchTerm.length > 0 && platform && !profiles?.length) return "👽 No Results. Please try different identity.";
     return "";
   })();
   function PlatformFilter({ platforms, onSelectChange }: { platforms: string[]; onSelectChange: (v: string) => void }) {
@@ -63,7 +65,7 @@ export default function Command() {
       navigationTitle="Web3.bio"
       searchBarPlaceholder="Search Ethereum (ENS), Lens, Farcaster, UD..."
       onSearchTextChange={setSearchTerm}
-      throttle
+      throttle={false}
       searchBarAccessory={
         <PlatformFilter
           platforms={(!profiles?.length || profiles.error ? [] : profiles)?.map((x: Profile) => x.platform)}
@@ -77,80 +79,98 @@ export default function Command() {
       }
     >
       <List.EmptyView title={emptyText} icon={"logo-web3bio.png"} />
-      {profiles
-        ?.filter((x: Profile) => {
-          if (filter === "ALL") return x;
-          return x.platform === filter;
-        })
-        ?.map((item: Profile) => (
-          <List.Item
-            key={item.identity + item.platform}
-            title={item.identity}
-            subtitle={item.platform?.toUpperCase()}
-            icon={{ source: item.avatar || "", mask: Image.Mask.Circle, fallback: "logo-web3bio.png" }}
-            actions={
-              <ActionPanel>
-                <Action.Push
-                  title="Profile Detail"
-                  icon={Icon.AppWindowSidebarLeft}
-                  target={
-                    <ProfileResults
-                      profiles={[item, ...profiles.filter((x: Profile) => item !== x)]}
-                      searchTerm={item.identity}
-                    />
-                  }
-                />
-              </ActionPanel>
-            }
-          />
-        ))}
+      <List.Section title="Profiles">
+        {profiles
+          ?.filter((x: Profile) => {
+            if (filter === "ALL") return x;
+            return x.platform === filter;
+          })
+          ?.map((item: Profile) => (
+            <List.Item
+              key={item.identity + item.platform}
+              title={item.displayName || item.identity}
+              subtitle={item.displayName && item.displayName === item.identity ? "" : item.identity}
+              icon={{ source: item.avatar || "", mask: Image.Mask.Circle }}
+              accessories={[
+                { text: SocialPlatformMapping(item.platform as PlatformType).label, icon: SocialPlatformMapping(item.platform as PlatformType).icon },
+              ]}
+              actions={
+                <ActionPanel>
+                  <Action.Push
+                    title="Profile Detail"
+                    icon={Icon.AppWindowSidebarLeft}
+                    target={
+                      <ProfileResults
+                        profiles={[item, ...profiles.filter((x: Profile) => item !== x)]}
+                        searchTerm={item.identity}
+                      />
+                    }
+                  />
+                </ActionPanel>
+              }
+              
+            />
+          ))}
+      </List.Section>
     </List>
   );
   function ProfileResults({ profiles, searchTerm }: { profiles: Profile[]; searchTerm: string }) {
     const { pop } = useNavigation();
     return (
       <List isShowingDetail searchBarPlaceholder={searchTerm} onSearchTextChange={() => pop()}>
-        <List.Section title="Related Profiles">
+        <List.Section title="Profiles">
           {profiles.map((x: Profile) => {
+            const relatedPath = `${x.identity}${
+              x.platform === PlatformType.farcaster ? ".farcaster" : ""
+            }`;
             return (
               <List.Item
                 key={`item_detailed_${x.identity}_${x.platform}`}
-                title={x.identity}
-                subtitle={x.platform?.toUpperCase()}
-                icon={{ source: x.avatar || "", mask: Image.Mask.Circle, fallback: "logo-web3bio.png" }}
+                title={x.displayName || x.identity}
+                subtitle={x.displayName && x.displayName === x.identity ? "" : x.identity}
+                icon={{ source: x.avatar || "", mask: Image.Mask.Circle }}
+                accessories={[
+                  { text: SocialPlatformMapping(x.platform as PlatformType).label, icon: SocialPlatformMapping(x.platform as PlatformType).icon },
+                ]}
                 detail={
                   <List.Item.Detail
+                    markdown={`![Avatar](${x.avatar}?raycast-width=120&raycast-height=120)`}
                     metadata={
                       <List.Item.Detail.Metadata>
-                        <List.Item.Detail.Metadata.Label title="#️⃣ Basic" />
-                        <List.Item.Detail.Metadata.Separator />
-                        <List.Item.Detail.Metadata.Label title="Address" text={x.address} />
-                        <List.Item.Detail.Metadata.Label title="Platform" text={x.platform} />
-                        <List.Item.Detail.Metadata.Label title="DisplayName" text={x.displayName || ""} />
-                        {x.email && <List.Item.Detail.Metadata.Label title="Email" text={x.email || ""} />}
-                        {x.description && (
-                          <List.Item.Detail.Metadata.Label title="Description" text={x.description || ""} />
-                        )}
-                        {x.location && <List.Item.Detail.Metadata.Label title="Location" text={x.location || ""} />}
-                        {x.header && (
+                        {x.avatar && (
                           <List.Item.Detail.Metadata.Label
-                            title="Header"
-                            icon={{ source: x.header, mask: Image.Mask.RoundedRectangle, fallback: "logo-web3bio.png" }}
+                            title=""
+                            icon={{ source: x.avatar, mask: Image.Mask.Circle }}
                           />
                         )}
-
-                        {Object.keys(x.links || {})?.length > 0 && (
+                        {x.displayName === x.identity ? (
+                          <List.Item.Detail.Metadata.Label title={x.displayName} />
+                        ) : (
                           <>
-                            <List.Item.Detail.Metadata.Label title="🌐 Social Medias" />
+                            <List.Item.Detail.Metadata.Label title={x.displayName || ""} />
+                            <List.Item.Detail.Metadata.Label title={x.identity} />
+                          </>
+                        )} 
+                        <List.Item.Detail.Metadata.Label title={x.address} />
+                        <List.Item.Detail.Metadata.Label title="Platform" text={SocialPlatformMapping(x.platform as PlatformType).label} icon={SocialPlatformMapping(x.platform as PlatformType).icon} />
+                        {x.description && (
+                          <List.Item.Detail.Metadata.Label title="Description" text={x.description} />
+                        )}
+                        {x.email && <List.Item.Detail.Metadata.Label title="Email" text={x.email} />}
+                        {x.location && <List.Item.Detail.Metadata.Label title="Location" text={x.location} />}
+                        
+
+                        {Object.keys(x.links)?.length > 0 && (
+                          <>
                             <List.Item.Detail.Metadata.Separator />
 
-                            {Object.keys(x.links || {}).map((i) => {
-                              const item = x.links[i];
+                            {Object.keys(x.links).map((key) => {
+                              const item = x.links[key as PlatformType];
                               return (
                                 item.handle && (
                                   <List.Item.Detail.Metadata.Link
-                                    key={`${item.handle}_${item.link}`}
-                                    title={i}
+                                    key={`${key}_${item.handle}`}
+                                    title={SocialPlatformMapping(key as PlatformType).label}
                                     text={item.handle}
                                     target={item.link}
                                   />
@@ -166,8 +186,8 @@ export default function Command() {
                 actions={
                   <ActionPanel>
                     <Action.OpenInBrowser
-                      title="Open in Web3.bio Profile page"
-                      url={"https://web3.bio/" + x.identity}
+                      title="Open in Web3.bio Profile"
+                      url={"https://web3.bio/" + relatedPath}
                     />
                   </ActionPanel>
                 }
